00005c8dc06c98097f89c9b5ef3d4bf1b87d2eac
Michael Schwarze <michael@schwarze-web.de>
2015-04-21 12:45:20 +0200

    Anzeige Wahl des Computers; weitere Refactoring

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # SchnickSchnackSchnuck
 ## Schere, Stein, Papier in JavaScript
 Absolute Minimalversion, nur Logik; offen:
-* Anzeige Wahl des Computers
 * Ein-/Ausgabe per HTML/CSS
     * mit Radio-Button
     * mit Bildern
--- SchnickSchnackSchnuck.js
+++ SchnickSchnackSchnuck.js
@@ -4,7 +4,7 @@
 /* Funktionsblock */
 /* -------------- */
 
-/* vergleich vergleicht die Eingaben und gibt den Gewinner zurück */
+/* "vergleich" vergleicht die Eingaben und gibt das gewinnende Element zurück */
 
 var vergleich = function (auswahl1, auswahl2) {
 	auswahl1 = auswahl1.toLowerCase().trim();
@@ -36,17 +36,22 @@ var vergleich = function (auswahl1, auswahl2) {
     }
 };
 
+var erzeugeComputerAuswahl = function () {
+	var zufallsZahl = Math.random();
+	if (zufallsZahl < 0.34) {
+		return "stein";
+	} else if(zufallsZahl <= 0.67) {
+		return "papier";
+	} else {
+		return "schere";
+	} 	
+}
+
 /* Hauptteil */
 /* --------- */
 
-var auswahl = prompt("Bitte Schere, Stein oder Papier eingeben:");
-var computerAuswahl = Math.random();
-if (computerAuswahl < 0.34) {
-	computerAuswahl = "stein";
-} else if(computerAuswahl <= 0.67) {
-	computerAuswahl = "papier";
-} else {
-	computerAuswahl = "schere";
-} 
-
-alert("Der Computer hatte " + computerAuswahl + ". " + vergleich(auswahl, computerAuswahl));
+var spielerAuswahl = prompt("Bitte Schere, Stein oder Papier eingeben:");
+var computerAuswahl = erzeugeComputerAuswahl();
+var meldung = "Du hast " + spielerAuswahl.substr(0, 1).toUpperCase() + spielerAuswahl.substr(1) + " und der Computer hat " + computerAuswahl.substr(0, 1).toUpperCase() + computerAuswahl.substr(1) + ".\n";
+meldung = meldung + vergleich(spielerAuswahl, computerAuswahl);
+alert(meldung);
